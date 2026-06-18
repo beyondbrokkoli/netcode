d9bededf0f7a4d38d367c25278d647ed4d8fc6af
beyondbrokkoli <halim.zag@gmx.de>
2026-06-18 22:31:39 +0200

    m

--- main.lua
+++ main.lua
@@ -203,26 +203,32 @@ print(string.format("[SYSTEM] Assigning Identity: Node %d. Meshing topology...",
 local p2p_established = {}
 local active_peers = {}
 
--- [!] SSoT: Topology Evaluation
-if p.ip == my_pub_ip and p.local_ip == my_local_ip then
-    net.Connect(peer_id, "127.0.0.1", tonumber(p.local_port))
-    if my_pub_ip == "127.0.0.1" or p.ip == "127.0.0.1" then
-        p2p_established[peer_id] = true -- Auto-trust localhost harness
-        print(string.format("[ICE] Node %d is Localhost Harness. Auto-validated.", peer_id))
-    else
-        print(string.format("[ICE] Node %d is local loopback. Attempting blast...", peer_id))
+for i, p in ipairs(status_data.players) do
+    local peer_id = i - 1
+    if peer_id ~= local_id then
+        active_peers[peer_id] = true
+        -- [!] SSoT: Topology Evaluation
+        if p.ip == my_pub_ip and p.local_ip == my_local_ip then
+            net.Connect(peer_id, "127.0.0.1", tonumber(p.local_port))
+            if my_pub_ip == "127.0.0.1" or p.ip == "127.0.0.1" then
+                p2p_established[peer_id] = true -- Auto-trust localhost harness
+                print(string.format("[ICE] Node %d is Localhost Harness. Auto-validated.", peer_id))
+            else
+                print(string.format("[ICE] Node %d is local loopback. Attempting blast...", peer_id))
+            end
+        elseif p.ip == my_pub_ip then
+            net.Connect(peer_id, p.local_ip, tonumber(p.local_port))
+            if my_pub_ip == "127.0.0.1" or p.ip == "127.0.0.1" then
+                p2p_established[peer_id] = true -- Auto-trust localhost harness
+                print(string.format("[ICE] Node %d is Localhost Harness. Auto-validated.", peer_id))
+            else
+                print(string.format("[ICE] Node %d is on LAN. Attempting hairpin bypass...", peer_id))
+            end
+        else
+            net.Connect(peer_id, p.ip, tonumber(p.port))
+            print(string.format("[ICE] Node %d is on WAN. Attempting NAT punch...", peer_id))
+        end
     end
-elseif p.ip == my_pub_ip then
-    net.Connect(peer_id, p.local_ip, tonumber(p.local_port))
-    if my_pub_ip == "127.0.0.1" or p.ip == "127.0.0.1" then
-        p2p_established[peer_id] = true -- Auto-trust localhost harness
-        print(string.format("[ICE] Node %d is Localhost Harness. Auto-validated.", peer_id))
-    else
-        print(string.format("[ICE] Node %d is on LAN. Attempting hairpin bypass...", peer_id))
-    end
-else
-    net.Connect(peer_id, p.ip, tonumber(p.port))
-    print(string.format("[ICE] Node %d is on WAN. Attempting NAT punch...", peer_id))
 end
 
 local real_time_remaining = status_data.start_time - status_data.server_time
